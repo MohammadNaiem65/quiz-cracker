dc0fb6f33549d77bb2465785e49961675a923b85
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2022-10-12 03:55:16 +0600

    created the header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import ErrorPage from './components/utilities/ErrorPage';
+import Root from './components/utilities/Root';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/', element: <Root></Root>, errorElement: <ErrorPage></ErrorPage>, children: [
+      { path: '/', element: <Home></Home>, index: true }
+    ]
+  }
+])
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import { faBars, faClipboardList } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='w-11/12 mx-auto mt-6 flex justify-between items-center'>
+            <div onClick={() => <Link to='/home'></Link>} className='flex items-center'>
+                <FontAwesomeIcon className='text-3xl mr-9 mt-1 cursor-pointer hover:text-[#A66CFF]' icon={faBars}></FontAwesomeIcon>
+                <div className='flex  cursor-pointer'>
+                    <FontAwesomeIcon className='text-4xl text-[#A66CFF]' icon={faClipboardList}></FontAwesomeIcon>
+                    <h1 className='text-3xl font-bold text-[#A66CFF] ml-2'>QuizAdda</h1>
+                </div>
+            </div>
+            <div>
+                <button className='text-lg text-[#A66CFF] px-3 py-1 font-bold border-2 rounded-lg border-[#A66CFF] hover:bg-[#A66CFF] hover:text-white'>Take A Quiz</button>
+                <button className='text-lg ml-5 px-3 py-1 font-bold text-white bg-[#A66CFF] rounded-lg hover:bg-white hover:text-[#A66CFF] hover:border-2 border-[#A66CFF]'>Blogs</button>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
